4e365243a8680d901a0d2debcf24d314b6dc477b
codal-mchandarana <mchandarana@codal.com>
2024-02-22 18:35:03 +0530

    Commiting new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import FileUpload from "@/Components/FirstPage/FileUpload";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <Upload />
       <FileUpload />
     </main>
